1ba7c87313aa29a03fc868cd4f1052fe3a7615ce
ju-baer <108940924+ju-baer@users.noreply.github.com>
2022-07-08 20:27:03 +0600

    Create minmax.py

---
+++ minmax.py
@@ -0,0 +1,24 @@
+def largest(*list):
+    """ This function can return the max value of a given tuple
+    Args:
+        list (tuple)
+    Returns:
+        _type_: Max of Tuple
+    """  
+    return max(list)
+
+
+def smallest(*list):
+    """ This function can return the min value of a given tuple
+    Args:
+        list (tuple)
+    Returns:
+        _type_: Max of Tuple
+    """    
+    return min(list)
+
+
+if __name__ == '__main__':
+    print(largest(0,9,28,6,-1,2))
+    print(smallest(0,9,28,6,-1,2))
+
